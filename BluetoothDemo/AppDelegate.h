012e4820c94845acb410bf750368836f6c8f7e4f
Dan F <tazfreak294@hotmail.com>
2012-08-21 11:17:06 -0400

    Initial Commit

---
+++ BluetoothDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BluetoothDemo
+//
+//  Created by Dan Fellmeth on 8/21/12.
+//  Copyright (c) 2012 Globus Medical Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BluetoothDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BluetoothDemo
+//
+//  Created by Dan Fellmeth on 8/21/12.
+//  Copyright (c) 2012 Globus Medical Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
